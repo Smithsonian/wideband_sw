d3b88216339c6d3aecb507b8abf0f6ff1ab56405
Rurik A Primiani <rprimian@cfa.harvard.edu>
2013-05-10 01:26:53 +0000

    [wideCrate] Separated the Walsh sync procedure into its own function.
    This allows it to now be called multiple times using pthread to
    arm many ROACH's simultaneously. This is now done in the handle_package
    function for two hard-coded ROACH2's.
    
    Another change was to comment out the RPC server svc_freeargs call.
    While trying to copy the package argument into a global variable so that
    other threads could access it I kept getting seg. faults but couldn't
    figure out why. The copy routine I wrote should have worked but the
    package was being freed and there were still pointers pointing to that
    freed memory somewhere! Instead of dealing with that I just removed the
    freeing. Probably not the best approach but since this isn't actually
    running as a server it's probably okay.

--- wideCrate/wideCrate.c
+++ wideCrate/wideCrate.c
@@ -30,6 +30,57 @@ volatile sig_atomic_t package_received = 0;
 /* Global package shared between threads */
 intgWalshPackage *dds_package;
 
+typedef struct {
+  char roach2_hostname[64];
+  struct timespec sowf_ts;
+  int hb_offset;
+} send_walsh_sync_args;
+
+typedef struct {
+  int errCode;
+} send_walsh_sync_resp;
+
+void* send_walsh_sync(void* arg_ptr)
+{
+  int i, total, katcp_result;
+  struct katcl_line *l;
+  send_walsh_sync_resp* resp = malloc(sizeof(send_walsh_sync_resp));
+  send_walsh_sync_args* args = (send_walsh_sync_args *)arg_ptr;
+
+  /* connect to roach2 */
+  l = create_name_rpc_katcl(args->roach2_hostname);
+  if(l == NULL){
+    fprintf(stderr, "unable to create client connection to %s: %s\n", 
+	    args->roach2_hostname, strerror(errno));
+    resp->errCode = ERROR;
+    return (void *)resp;
+  }
+
+  /* send the walsh arm request, with 60s timeout */
+  katcp_result = send_rpc_katcl(l, 60000,
+  				KATCP_FLAG_FIRST | KATCP_FLAG_STRING, "?sma-walsh-arm",
+  				KATCP_FLAG_ULONG, args->sowf_ts.tv_sec,
+  				KATCP_FLAG_ULONG, args->sowf_ts.tv_nsec,
+  				KATCP_FLAG_LAST  | KATCP_FLAG_SLONG, (long)args->hb_offset,
+  				NULL);
+
+  /* result is 0 if the reply returns "ok", 1 if it failed and -1 if things went wrong doing IO or otherwise */
+  printf("result of ?sma-walsh-arm is %d\n", katcp_result);
+
+  /* you can examine the content of the reply with the following functions */
+  total = arg_count_katcl(l);
+  printf("have %d arguments in reply\n", total);
+  for(i = 0; i < total; i++){
+    /* for binary data use the arg_buffer_katcl, string will stop at the first occurrence of a \0 */
+    printf("reply[%d] is <%s>\n", i, arg_string_katcl(l, i));
+  }
+
+  destroy_rpc_katcl(l);
+
+  resp->errCode = OK;
+  return (void *)resp;
+}
+
 statusCheck(dDSStatus *status)
 {
   if (status->status == DDS_SUCCESS)
@@ -80,17 +131,16 @@ void intgprog_1(struct svc_req *rqstp, register SVCXPRT *transp)
   if (result != NULL && !svc_sendreply(transp, (xdrproc_t) _xdr_result, result)) {
     svcerr_systemerr (transp);
   }
-  if (!svc_freeargs (transp, (xdrproc_t) _xdr_argument, (caddr_t) &argument)) {
-    fprintf (stderr, "%s", "unable to free arguments");
-    exit (1);
-  }
+  /* if (!svc_freeargs (transp, (xdrproc_t) _xdr_argument, (caddr_t) &argument)) { */
+  /*   fprintf (stderr, "%s", "unable to free arguments"); */
+  /*   exit (1); */
+  /* } */
   return;
 }
 
 intgWalshResponse *result;
 intgWalshResponse *intgsyncwalsh_1(intgWalshPackage *package, CLIENT *cl)
 {
-  int i, j, pattern_len, step_len;
 
   result = malloc(sizeof(intgWalshResponse));
   if (result == NULL) {
@@ -100,27 +150,7 @@ intgWalshResponse *intgsyncwalsh_1(intgWalshPackage *package, CLIENT *cl)
 
   if (!package_received) {
     pthread_mutex_lock(&mutex);
-    dds_package = package;
-
-    dds_package->pattern = package->pattern;
-    pattern_len = dds_package->pattern.pattern_len;
-    dds_package->pattern.pattern_val = malloc(pattern_len * sizeof(intgWalshPattern));
-    for (i=0; i<pattern_len; i++) {
-      dds_package->pattern.pattern_val[i] = package->pattern.pattern_val[i];
-      dds_package->pattern.pattern_val[i].step = package->pattern.pattern_val[i].step;
-
-      step_len = dds_package->pattern.pattern_val[i].step.step_len;
-      if (step_len == 0) {
-	dds_package->pattern.pattern_val[i].step.step_val = NULL;
-      } else {
-	dds_package->pattern.pattern_val[i].step.step_val = malloc(step_len * sizeof(int));
-      }
-      for (j=0; j<step_len; j++) {
-	printf("%d:%d", i, j);
-	dds_package->pattern.pattern_val[i].step.step_val[j] = package->pattern.pattern_val[i].step.step_val[j];
-      }
-    }
-
+    dds_package = package; // only possible since I disable svc_freeargs
     pthread_mutex_unlock(&mutex);
     package_received = 1;
   }
@@ -130,31 +160,37 @@ intgWalshResponse *intgsyncwalsh_1(intgWalshPackage *package, CLIENT *cl)
 }
 
 int handle_package(intgWalshPackage *package, int hb_offset) {
-  struct katcl_line *l;
-  int i, nPatterns, step_len, total, katcp_result;
+  send_walsh_sync_args *args1 = malloc(sizeof(send_walsh_sync_args));
+  send_walsh_sync_args *args2 = malloc(sizeof(send_walsh_sync_args));
+  send_walsh_sync_resp *resp1, *resp2;
+  void *respVoid1, *respVoid2;
+  pthread_t tid1, tid2;
+
+  int i, nPatterns, step_len;
   struct timespec sowf_ts, armed_at;
   struct tm curr;
   struct tm sowf;
   time_t raw;
 
+
   /* Process the Walsh table */
   printf("Received a package from the DDS Server - contents:\n");
   nPatterns = package->pattern.pattern_len;
-  /* printf("Number of Walsh Patterns %d, listed below\n", nPatterns); */
-  /* for (i = 1; i < nPatterns; i++) { */
-  /*   int j; */
+  printf("Number of Walsh Patterns %d, listed below\n", nPatterns);
+  for (i = 1; i < nPatterns; i++) {
+    int j;
 
-  /*   step_len = package->pattern.pattern_val[i].step.step_len; */
-  /*   printf("%2d: %d ", i, step_len); */
-  /*   for (j = 0; j < step_len; j++) */
-  /*     printf("%d", package->pattern.pattern_val[i].step.step_val[j]); */
-  /*   printf("\n"); */
-  /* } */
+    step_len = package->pattern.pattern_val[i].step.step_len;
+    printf("%2d: %d ", i, step_len);
+    for (j = 0; j < step_len; j++)
+      printf("%d", package->pattern.pattern_val[i].step.step_val[j]);
+    printf("\n");
+  }
 
   /* Print a human-readable version of the agreed upon start of Walsh hearbeat */
   printf("Start at day %d of year %d\n", package->startDay, package->startYear);
   printf("at %02d:%02d:%02d.%06d\n", package->startHour, package->startMin,
-	 package->startSec, package->startuSec);
+  	 package->startSec, package->startuSec);
   
   /* Get a struct tm of current time and replace with SOWF info */
   time(&raw);
@@ -163,8 +199,8 @@ int handle_package(intgWalshPackage *package, int hb_offset) {
   sowf.tm_year = package->startYear - 1900;
   sowf.tm_yday = package->startDay;
   sowf.tm_hour = package->startHour;
-  sowf.tm_min = package->startMin;
-  sowf.tm_sec = package->startSec;
+  sowf.tm_min  = package->startMin;
+  sowf.tm_sec  = package->startSec;
   printf("Current time: %d\n", (int)mktime(&curr));
   printf("SOWF time:    %d\n", (int)mktime(&sowf));
   
@@ -172,39 +208,31 @@ int handle_package(intgWalshPackage *package, int hb_offset) {
   sowf_ts.tv_sec = mktime(&sowf);
   sowf_ts.tv_nsec = (package->startuSec) * 1e3;
 
-  /* connect to roach2-02 */
-  l = create_name_rpc_katcl("roach2-02");
-  if(l == NULL){
-    fprintf(stderr, "unable to create client connection to roach2-02: %s\n", strerror(errno));
-    result->status = ERROR;
-    return ERROR;
-  }
+  /* Send SOWF into to first ROACH */
+  args1->sowf_ts = sowf_ts;
+  args1->hb_offset = hb_offset;
+  strcpy(args1->roach2_hostname, "roach2-01");
+  pthread_create(&tid1, NULL, send_walsh_sync, (void *)args1);
+  printf("Sent SOWF to roach2-01\n");
 
-  /* send the walsh arm request, with 60s timeout */
-  katcp_result = send_rpc_katcl(l, 60000,
-  				KATCP_FLAG_FIRST | KATCP_FLAG_STRING, "?sma-walsh-arm",
-  				KATCP_FLAG_ULONG, sowf_ts.tv_sec,
-  				KATCP_FLAG_ULONG, sowf_ts.tv_nsec,
-  				KATCP_FLAG_LAST  | KATCP_FLAG_SLONG, (long)hb_offset,
-  				NULL);
+  /* Send SOWF into to second ROACH */
+  args2->sowf_ts = sowf_ts;
+  args2->hb_offset = hb_offset;
+  strcpy(args2->roach2_hostname, "roach2-02");
+  pthread_create(&tid2, NULL, send_walsh_sync, (void *)args2);
+  printf("Sent SOWF to roach2-02\n");
 
+  /* Join all threads, wait for return */
+  pthread_join(tid1, respVoid1);
+  pthread_join(tid2, respVoid2);
+  resp1 = (send_walsh_sync_resp *)respVoid1;
+  resp2 = (send_walsh_sync_resp *)respVoid2;
+
+  /* After functions return */
   clock_gettime(CLOCK_REALTIME, &armed_at);
   printf("Presumably armed at: %d.%06d\n", (int)armed_at.tv_sec, (int)armed_at.tv_nsec);
 
-  /* result is 0 if the reply returns "ok", 1 if it failed and -1 if things went wrong doing IO or otherwise */
-  printf("result of ?sma-walsh-arm is %d\n", katcp_result);
-
-  /* you can examine the content of the reply with the following functions */
-  total = arg_count_katcl(l);
-  printf("have %d arguments in reply\n", total);
-  for(i = 0; i < total; i++){
-    /* for binary data use the arg_buffer_katcl, string will stop at the first occurrence of a \0 */
-    printf("reply[%d] is <%s>\n", i, arg_string_katcl(l, i));
-  }
-
-  destroy_rpc_katcl(l);
-
-  return OK;
+  return resp1->errCode;
 }
 
 intgWalshResponse *intgsyncwalsh_1_svc(intgWalshPackage *package,
@@ -283,7 +311,6 @@ int main(int argc, char **argv)
   command.client[19] = (char)0;
   printf("This client's name is \"%s\".\n", command.client);
   statusCheck(ddsrequest_1(&command, cl));
-  //pthread_join(serverTId, NULL);
 
   while (!package_received) {
     usleep(1);
@@ -294,4 +321,6 @@ int main(int argc, char **argv)
   }
 
   handle_package(dds_package, hb_offset);
+
+  //pthread_join(serverTId, NULL);
 }
