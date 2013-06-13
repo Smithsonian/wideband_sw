a5e274031d55e956762b1286b6d021809bb623a2
Ken Young <orrery.moko@gmail.com>
2013-06-13 16:17:45 -0400

    Trivial program to allow remote rebooting of the roaches

---
+++ roach2root/application/rebooter/rebooter.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void signalHandler(int signum)
+{
+  if ((signum == SIGURG) || (signum == SIGSTKFLT)) {
+    int errCode;
+
+    fprintf(stderr, "rebooter received signal %d - will try to force a reboot\n", signum);
+    errCode = execl("/bin/bash", "bash", "/common/bin/shutdown", (char *)NULL);
+    perror("execl");
+    fprintf(stderr, "execl returned %d\n", errCode);
+  } else
+    fprintf(stderr, "rebooter signalhandler received an unexpected signum = %d - will ignore\n",
+            signum);
+}
+
+main()
+{
+  struct sigaction action, oldAction;
+
+  action.sa_flags = 0;
+  sigemptyset(&action.sa_mask);
+  action.sa_handler = signalHandler;
+  sigaction(SIGURG, &action, &oldAction);
+  sigaction(SIGSTKFLT, &action, &oldAction);
+  while (1)
+    sleep(36000);
+}
