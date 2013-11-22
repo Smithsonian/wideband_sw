81001f9fa0b9740f3e133e04ac00b15b70039e1e
Rurik A Primiani <rprimian@cfa.harvard.edu>
2013-11-22 06:53:39 -1000

    [scripts/xeng_order] Changed the mapping of antennas in xeng_order.
    This currently matches the direct-connect setup at the SMA.

--- scripts/xeng_order.py
+++ scripts/xeng_order.py
@@ -3,20 +3,20 @@
 import decode
 
 
-ants  = (0, 1, 2, 3)
+ants  = (2, 5, 6, 7)
 chans = (0, 8, 16, 24)
 direct_connect_mapping = (
-    ((0,  0), (1,  0)), # roach2-00: {input 0: [ant, chanl], input 1: [...]}
-    ((2,  0), (3,  0)), # roach2-01: {input 0: [...], input 1: [...]}
-    ((0,  8), (1,  8)), # roach2-00: {...}
-    ((2,  8), (3,  8)), # ...
-    ((0, 16), (1, 16)), # ...
-    ((2, 16), (3, 16)), # ...
-    ((0, 24), (1, 24)), # ...
-    ((2, 24), (3, 24)), # roach2-01: {...}
+    ((2,  0), (5,  0)), # roach2-00: {input 0: [ant, chanl], input 1: [...]}
+    ((6,  0), (7,  0)), # roach2-01: {input 0: [...], input 1: [...]}
+    ((2,  8), (5,  8)), # roach2-00: {...}
+    ((6,  8), (7,  8)), # ...
+    ((2, 16), (5, 16)), # ...
+    ((6, 16), (7, 16)), # ...
+    ((2, 24), (5, 24)), # ...
+    ((6, 24), (7, 24)), # roach2-01: {...}
 )
 
-visib_order = dict(((a, b), [0]*4) for a in range(4) for b in range(4) if b >= a)
+visib_order = dict(((a, b), [0]*4) for a in ants for b in ants if b >= a)
 out = list(decode.get_xengine_output_order(mapping=direct_connect_mapping))
 
 for i, ((left, left_chan), (right, right_chan)) in enumerate(out):
