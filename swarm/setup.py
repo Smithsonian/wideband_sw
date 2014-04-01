49406937b51a9cdb263e51a9f4082b57fe85448d
Rurik A Primiani <rprimian@cfa.harvard.edu>
2014-04-01 09:42:43 -1000

    [swarm] Added a simple setuptools setup script.
    Recommended usage is as folows:
    
    $ python setup.py develop
    
    This points python to the repository (instead of installing it to the
    Python install path) so that changes can be made without needed to re-run
    the installation.

---
+++ swarm/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "swarm",
+    version = "0.0.1",
+    author = "Rurik A Primiani",
+    author_email = "rprimian@cfa.harvard.edu",
+    description = "Monitor and control software for the SWARM correlator",
+    packages=['swarm',],
+)
