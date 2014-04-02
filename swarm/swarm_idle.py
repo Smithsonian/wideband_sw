89196c0f44d20d7d6278da3181d936e5bd17014d
Rurik A Primiani <rprimian@cfa.harvard.edu>
2014-04-02 05:31:28 -1000

    [swarm_idle] Added a script to quickly idle a Swarm.

---
+++ swarm/swarm_idle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2.7
+
+import logging
+from swarm import *
+
+# Setup some basic logging
+logging.basicConfig()
+formatter = logging.Formatter('%(name)-24s: %(asctime)s : %(levelname)-8s %(message)s')
+logger = logging.getLogger()
+logger.handlers[0].setFormatter(formatter)
+logger.setLevel(logging.INFO)
+
+def idlize(fid, member):
+    member.roach2.progdev('sma_idle.bof')
+    logger.info("Idling {} = FID #{}".format(member.roach2_host, fid))
+
+s = Swarm()
+s.members_do(idlize)
