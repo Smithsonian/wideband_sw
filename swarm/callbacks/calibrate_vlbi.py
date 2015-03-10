2446badcdc53e61199bba045834b67ddf362e981
Rurik A Primiani <rprimian@cfa.harvard.edu>
2015-03-10 10:54:52 -1000

    [callbacks/calibrate_vlbi] Added phase to the per-channel complex gain solver.
    In other words it now uses the lag-space to find the delay *and* the phase of the solved
    complex per-antenna, per-channel gain. Before it was just calculating the phase by
    averaging the band and finding the angle of that. Now it actually finds the angle of the
    peak in the lag-space.
    
    This does not work very well since we will need to either pad the IFFT to get a finer
    resolution on lag-space or as Laura's suggested just taking a sub-sample around the
    peak lag and interpolating.

--- swarm/callbacks/calibrate_vlbi.py
+++ swarm/callbacks/calibrate_vlbi.py
@@ -1,12 +1,13 @@
 from functools import partial
 from itertools import combinations_with_replacement as combos
 from numpy.linalg import eig as eig
-from numpy.fft import ifft, fftfreq
+from numpy.fft import ifft, fftshift
 from numpy import (
     nan_to_num,
     complex128,
     array,
     zeros,
+    arange,
     around,
     angle,
     isnan,
@@ -30,12 +31,15 @@ def solve_cgains(mat, ref=0):
     factor = ref_gain.conj() / abs(ref_gain + 1.0)
     return raw_gains * nan_to_num(factor)
 
-def solve_delay_phase(gains, chan_axis=0, pad_by=16):
+def solve_delay_phase(gains, chan_axis=0, sub_fft_size=16):
+    fft_size = gains.shape[chan_axis]
     samp_time_ns = 1e9 / (SWARM_CLOCK_RATE * 8.0)
-    fft_size = pad_by * gains.shape[chan_axis]
     lags = ifft(gains, n=fft_size, axis=chan_axis)
-    peaks = lags.real.argmax(axis=chan_axis)
-    return (1.0/pad_by) * peaks * samp_time_ns
+    bins = fftshift(arange(-fft_size/2, fft_size/2))
+    phases = angle(lags.max(axis=chan_axis))
+    peaks = lags.argmax(axis=chan_axis)
+    delays = -bins[peaks] * samp_time_ns
+    return delays, phases
 
 class CalibrateVLBI(SwarmDataCallback):
 
@@ -60,7 +64,7 @@ class CalibrateVLBI(SwarmDataCallback):
             corr_matrix[:, right_i, left_i] = complex_data.conj()
         referenced_solver = partial(solve_cgains, ref=inputs.index(self.reference))
         full_spec_gains = array(map(referenced_solver, corr_matrix))
-        delays = solve_delay_phase(full_spec_gains)
-        avg_gains = full_spec_gains.mean(axis=0)
+        delays, phases = solve_delay_phase(full_spec_gains)
+        amplitudes = abs(full_spec_gains).mean(axis=0)
         for i in range(len(inputs)):
-            self.logger.info('{} : Amp={:>12.2e}, Delay={:>8.2f} ns, Phase={:>8.2f} deg'.format(inputs[i], abs(avg_gains[i]), delays[i], (180.0/pi)*angle(avg_gains[i])))
+            self.logger.info('{} : Amp={:>12.2e}, Delay={:>8.2f} ns, Phase={:>8.2f} deg'.format(inputs[i], amplitudes[i], delays[i], (180.0/pi)*phases[i]))
