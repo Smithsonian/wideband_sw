03859497b78e9dc8fa13bf828b7eace41c88066a
Rurik A Primiani <rprimian@cfa.harvard.edu>
2016-10-03 04:45:17 -1000

    [swarm/ipython] The "config dual-rx" and "config single-rx" now don't touch quad 2 (or 3).
    This is because the IF for quadrant 2 has been rewired such that input 0 is always RxA and
    input 1 is always RxB (i.e. there is no longer an IF switch).

--- swarm/swarm/ipython/magics.py
+++ swarm/swarm/ipython/magics.py
@@ -27,10 +27,6 @@ class SwarmShellMagics(Magics):
             self.swarm[0].send_katcp_cmd('sma-astro-fstop-set', '7.85', '7.85', '-2.71359', '1', '1')
             self.swarm[0].set_walsh_patterns(swap90=[True, True])
 
-            # Set quadrant two to be dual-Rx, 6-8 GHz (with a flip)
-            self.swarm[1].send_katcp_cmd('sma-astro-fstop-set', '-8.15', '-8.15', '-2.71359', '1', '1')
-            self.swarm[1].set_walsh_patterns(swap90=[False, False])
-
             # Reset all the sideband separation tables
             self.swarm.set_sideband_states()
 
@@ -45,9 +41,6 @@ class SwarmShellMagics(Magics):
 
             # Set quadrant one to be single-Rx, 8-12 GHz (no flip in chunk 0, flip in chunk 1)
             self.swarm[0].send_katcp_cmd('sma-astro-fstop-set', '7.85', '-12.15', '-2.71359', '1', '1')
-
-            # Set quadrant two to be single-Rx, 4-8 GHz (no flip in chunk 2, flip in chunk 3)
-            self.swarm[1].send_katcp_cmd('sma-astro-fstop-set', '3.85', '-8.15', '-2.71359', '1', '1')
 
             # Reset all Walshing and sideband separation tables
             self.swarm.set_walsh_patterns()
