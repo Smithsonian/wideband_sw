617aa221d4df3a62b8018afd5e9333e30bf7d4d8
Rurik A Primiani <rprimian@cfa.harvard.edu>
2016-11-21 06:31:17 -1000

    [tbs_plugins] Added a shell script that can be sourced for cross-compiling on tenzing.
    To cross-compile the plugins, ssh to tenzing and do the following from your working copy
    of this repository:
    
    $ cd tbs_plugins
    $ source cross_defs.sh
    $ make clean && make swarm

---
+++ tbs_plugins/cross_defs.sh
@@ -0,0 +1,6 @@
+setenv PATH ${PATH}:/application/roach2_cross/bin
+setenv CROSS_COMPILE powerpc-linux-gnu-
+setenv GLOBAL /global
+setenv COMMON /usr/roach2common
+setenv ACCAPP /usr/roach2app
+setenv KATCPDIR ${ACCAPP}/src/katcp_devel
