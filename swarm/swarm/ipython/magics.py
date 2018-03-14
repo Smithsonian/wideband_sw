b4756e71d107cc40ab958bb476fe69c1b3f83cdf
anyoung <andre.young@cfa.harvard.edu>
2018-03-14 17:44:28 +0000

    [ipython/magics] Add beamformer macros
    Swarm shell now has the magics setbeam and showbeam available to allow
    rapid setting and checking of beamformer configuration.
    
    Set antennas in beam:
    ====================
    %setbeam <antenna-list> [ gain=<gain-val> ]
    
    <antenna-list> should evaluate to a list of antennas to include in the
    beam. The same beam is formed in each polarization / sideband.
    
    <gain-val> if supplied should evaluate to a scalar float. The same
    gain is applied uniformly to the beam in each polarization / sideband.
    If gain is not specified a value is automatically set, scaled by the
    inverse of the length of the antenna list.
    
    Examples:
    // include antennas 1 through 8 in the beam
    %setbeam range(8)
    // include only antenna 2 in beam
    %setbeam [2]
    // include antennas 1,2,3 in beam and set gain to 5.125
    %setbeam [1,2,3] gain=5.125
    
    Show antennas in beam:
    =====================
    %showbeam
    
    Prints a list of antennas in beam per chunk / sideband and shows the
    gain applied to each sideband / polarization.

--- swarm/swarm/ipython/magics.py
+++ swarm/swarm/ipython/magics.py
@@ -7,6 +7,9 @@ from IPython.core.magic import (
     line_cell_magic
     )
 
+from swarm.core import SwarmInput
+from swarm.defines import *
+
 @magics_class
 class SwarmShellMagics(Magics):
 
@@ -69,6 +72,66 @@ class SwarmShellMagics(Magics):
 
         else:
             self.logger.error("Calibration \"{0}\" not supported!".format(line))
+
+    @line_magic
+    def showbeam(self, line):
+
+        # Read SWARM beamformer configuration
+        inputs = self.swarm.get_beamformer_inputs()
+        gains = self.swarm.get_bengine_gains()
+        for ii,q in enumerate(inputs):
+            m = gains[ii][0]
+            print "qid{0}: gains = (RxA-USB = {1}, RxB-USB = {2}, RxA-LSB = {3}, RxB-LSB = {4})".format(ii,m[0],m[1],m[2],m[3])
+            for ii,sb in enumerate(q.keys()):
+                print "  |"
+                print "  +--{0}".format(sb)
+                for inp in q[sb]:
+                    if ii+1 < len(q.keys()):
+                        print "  |  +--{0}".format(inp)
+                    else:
+                        print "     +--{0}".format(inp)
+
+    @line_magic
+    def setbeam(self, line):
+
+        # Parse arguments
+        gain = None
+        idx_gain = line.find('gain=')
+        if idx_gain == -1:
+            ant_list = eval(line)
+        else:
+            ant_list = eval(line[:idx_gain])
+            gain_val = line[idx_gain:].split('=')[1]
+            if len(gain_val) > 0:
+                gain = eval(gain_val)
+
+        # Build beams structure
+        beams = [dict(zip(SWARM_BENGINE_SIDEBANDS,[[]]*len(SWARM_BENGINE_SIDEBANDS))),] * len(SWARM_MAPPING_CHUNKS)
+        for quad in self.swarm.quads:
+            qid = quad.qid
+            if hasattr(quad,'sdbe'):
+                sdbe = getattr(quad,'sdbe')
+                for sb in SWARM_BENGINE_SIDEBANDS:
+                    if sdbe.find(sb) != -1:
+                        beams[qid][sb] = [SwarmInput(ant, qid, pol) for pol in SWARM_MAPPING_POLS for ant in ant_list]
+
+        # Apply beams structure to SWARM
+        self.swarm.set_beamformer_inputs(beams)
+
+        # Maximum allowed gain value
+        MAX_GAIN = 15.9375
+        MIN_GAIN = 0.0
+
+        # If manual gain value not specified, set gains inversely proportional to the number of antennas in beam
+        if gain is None:
+            gain = MAX_GAIN / (len(ant_list))
+        elif gain > MAX_GAIN:
+            gain = MAX_GAIN
+        elif gain < MIN_GAIN:
+            gain = MIN_GAIN
+
+        # Apply gain
+        self.swarm.set_bengine_gains(gain, gain, gain, gain)
 
     @line_magic
     def setprompt(self, line):
