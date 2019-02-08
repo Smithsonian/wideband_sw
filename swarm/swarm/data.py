df5c333353c4ae713b6270331649f536dcf451c5
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2019-02-08 13:39:18 -0500

    Attempt to jit the get() method.

--- swarm/swarm/data.py
+++ swarm/swarm/data.py
@@ -86,8 +86,7 @@ class SwarmDataPackage(object):
     def get(self, *item):
         try:
             baseline, sideband = item
-            i = self.baselines_i[baseline]
-            j = SWARM_XENG_SIDEBANDS.index(sideband)
+            i, j = compiled_get(self.baselines_i, baseline, sideband)
             return self.array[i, j]
         except:
             raise KeyError("Please only index data package using [baseline, sideband]!")
@@ -128,6 +127,13 @@ class SwarmDataPackage(object):
         # Special case for autos, fill imag with zeros
         if baseline.is_auto():
             self.get(baseline, sideband)[slice_+1] = 0.0
+
+
+@jit
+def compiled_get(baselines_i, baseline, sideband):
+    i = baselines_i[baseline]
+    j = SWARM_XENG_SIDEBANDS.index(sideband)
+    return i, j
 
 
 @jit
