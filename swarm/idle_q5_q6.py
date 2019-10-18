4ac5581db701b9f3af338411dbce3b4eb79cd7c8
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2019-10-18 11:01:16 -0400

    Temporarily disabling the actual idle bitcode command to test script.

--- swarm/idle_q5_q6.py
+++ swarm/idle_q5_q6.py
@@ -10,13 +10,13 @@ Q5_Q6_MAPPINGS = [
 
 # Build two dictionaries one for active quadrant mappings and one for disabled quadrants.
 quad_mappings = OrderedDict()
-quad_mappings[5] = (Q5_Q6_MAPPINGS[0])
-quad_mappings[6] = (Q5_Q6_MAPPINGS[1])
+quad_mappings[5] = Q5_Q6_MAPPINGS[0]
+quad_mappings[6] = Q5_Q6_MAPPINGS[1]
 
 # Instantiate a Swarm object using the disabled quadrant mappings.
 swarm = Swarm(mappings_dict=quad_mappings)
 
 # IDLE the disabled quadrants.
 disabled_quad_string = " ".join(map(str, quad_mappings.keys()))
-pyopmess.send(1, 1, 100, "SWARM quadrant(s) " + disabled_quad_string + " now being idled")
-swarm.members_do(lambda fid, mbr: mbr.idle())
+# pyopmess.send(1, 1, 100, "SWARM quadrant(s) " + disabled_quad_string + " now being idled")
+# swarm.members_do(lambda fid, mbr: mbr.idle())
