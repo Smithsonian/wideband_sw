0065ce036da0e2d34f4da4176b8736788e4c9170
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2019-11-25 16:59:38 -0500

    testing out a swarm_integrate command.

---
+++ swarm/swarm_integrate.py
@@ -0,0 +1,35 @@
+#!/usr/local/anaconda/envs/swarm/bin/python
+import logging
+import sys
+from time import sleep
+import pyopmess
+from swarm import Swarm
+from swarm.defines import *
+from numpy import array
+# Setup root logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-30s: %(asctime)s : %(levelname)-8s %(message).140s')
+
+# Stream to stdout
+stdout = logging.StreamHandler(sys.stdout)
+stdout.setLevel(logging.INFO)
+logger.addHandler(stdout)
+
+
+swarm = Swarm()
+
+# Set the itime and wait for it to register
+logger.info('Setting integration time and resetting x-engines...')
+for fid, member in swarm.get_valid_members():
+    member.set_itime(30)
+
+# Reset the xengines until window counters to by in sync
+win_period = SWARM_ELEVENTHS * (SWARM_EXT_HB_PER_WCYCLE / SWARM_WALSH_SKIP)
+win_sync = False
+while not win_sync:
+    swarm.reset_xengines()
+    sleep(0.5)
+    win_count = array([m.roach2.read_uint('xeng_status') for f, m in swarm.get_valid_members()])
+    win_sync = len(set(c / win_period for c in win_count)) == 1
+    logger.info('Window sync: {0}'.format(win_sync))
