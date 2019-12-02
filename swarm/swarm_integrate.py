fe41c46db17109b1ee3d36c61349b6415ee7d9a7
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2019-12-02 16:04:04 -0500

    Added a param to the swarm_integrate script.

--- swarm/swarm_integrate.py
+++ swarm/swarm_integrate.py
@@ -1,11 +1,14 @@
 #!/usr/local/anaconda/envs/swarm/bin/python
 import logging
 import sys
+import argparse
 from time import sleep
 import pyopmess
 from swarm import Swarm
 from swarm.defines import *
 from numpy import array
+
+
 # Setup root logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -16,13 +19,22 @@ stdout = logging.StreamHandler(sys.stdout)
 stdout.setLevel(logging.INFO)
 logger.addHandler(stdout)
 
+# Set up command line parameter parsing.
+parser = argparse.ArgumentParser(description='Script to directly set the integration time for SWARM')
+parser.add_argument('time', type=float, default=30,
+                    help='Enter a time in seconds to apply to SWARM for integration time, can be floating point')
+args = parser.parse_args()
+
+if args.time < 0 or args.time == 0:
+    logger.error("Invalid integration time, must be greater than zero.")
+    sys.exit()
 
 swarm = Swarm()
 
 # Set the itime and wait for it to register
 logger.info('Setting integration time and resetting x-engines...')
 for fid, member in swarm.get_valid_members():
-    member.set_itime(30)
+    member.set_itime(args.time)
 
 # Reset the xengines until window counters to by in sync
 win_period = SWARM_ELEVENTHS * (SWARM_EXT_HB_PER_WCYCLE / SWARM_WALSH_SKIP)
