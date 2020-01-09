e939633cdf49d347a96f2b70c8168ffc3eb89857
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2020-01-09 11:56:55 -0500

    Added debug statement for every packet caught.

--- swarm/swarm/data.py
+++ swarm/swarm/data.py
@@ -319,6 +319,8 @@ class SwarmDataCatcher:
             xnum = determine_xnum(xnum_mb, xnum_lh)
             acc_n = determine_acc_n(acc_n_mb, acc_n_lh)
 
+            self.logger.debug("Caught packet %d from qid: %d fid: %d" % (pkt_n, qid, fid))
+
             # Initialize qid data buffer, if necessary
             if not data.has_key(qid):
                 data[qid] = {}
