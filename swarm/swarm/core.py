8faf9e45579529145ee0a43488fb980730b76f6c
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2020-08-20 15:41:15 -0400

    Revert "Removed the old_value portion."
    This reverts commit 1b93a1341a359c14ffdf608bc7fc5e0fba3eb038.

--- swarm/swarm/core.py
+++ swarm/swarm/core.py
@@ -308,7 +308,7 @@ class SwarmMember(base.SwarmROACH):
         old_xeng_time = self._xeng_itime
         self._xeng_itime = SWARM_ELEVENTHS * (SWARM_EXT_HB_PER_WCYCLE/SWARM_WALSH_SKIP) * int(round(itime_sec/SWARM_WALSH_PERIOD))
         try:
-            self.roach2.blindwrite(SWARM_XENG_CTRL, pack(SWARM_REG_FMT, self._xeng_itime & 0x1fffffff))
+            self.roach2.blindwrite(SWARM_XENG_CTRL, pack(SWARM_REG_FMT, (old_xeng_time & 0xe0000000) + self._xeng_itime & 0x1fffffff))
         except RuntimeError:
             self.logger.warning("Unable to set integration time on roach2")
 
