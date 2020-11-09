75e128a22f9a2a62b9e8e60a85bd34f623d165ac
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2020-11-09 12:44:23 -0500

    Converted test script to run on a thread.

--- swarm/cgains_test.py
+++ swarm/cgains_test.py
@@ -1,9 +1,21 @@
-from redis import StrictRedis, ConnectionError
+import redis
 
 redis_host = "localhost"
 redis_port = 6379
 key = "cgains-update"
-redis = StrictRedis(redis_host, redis_port)
-value_from_redis = redis.get(key)
 
-print(value_from_redis)
+
+def my_handler(message):
+    print('MY HANDLER: ', message['data'])
+
+
+redis_connection = redis.Redis(host=redis_host, port=redis_port)
+redis_pubsub = redis_connection.pubsub()
+redis_pubsub.psubscribe(**{'hello*': my_handler})
+thread = redis_pubsub.run_in_thread(sleep_time=1)
+
+
+# manually get value from key.
+# value_from_redis = redis.get(key)
+
+raw_input("Subscribed to channel, press any key to exit...")
