473c446677b08488baee50305c8b2269354310f0
Garrett (Karto) Keating <garrett.keating@cfa.harvard.edu>
2022-03-03 11:25:42 -1000

    [swarm/defines] Fixing the last redis bytes->str issue in the SWARM python code... I hope.

--- swarm/swarm/defines.py
+++ swarm/swarm/defines.py
@@ -23,7 +23,7 @@ def get_swarm_mappings():
     base_path = "/global/configFiles/swarmMapping.quad"
     vlbi_path = ""
     redis_client = Redis(host='localhost', port=6379)
-    vlbi_status = redis_client.get("vlbi")
+    vlbi_status = redis_client.get("vlbi").decode()
     if vlbi_status == "4to8":
         vlbi_path = ".vlbi4-8"
     elif vlbi_status == "5to9":
