3eb3637fb38bd1183e5abdf33492b836fedeeb0d
Garrett 'Karto' Keating <garrett.keating@cfa.harvard.edu>
2022-03-03 14:47:14 -0500

    [calibrate_vlbi] Some general code cleanup, making things more readable, also rearranging data matrix in solve_for so that the "fast" axis is along the spectral direction.

--- swarm/callbacks/calibrate_vlbi.py
+++ swarm/callbacks/calibrate_vlbi.py
@@ -7,6 +7,8 @@ from numpy.linalg import eig as eig
 from numpy.fft import fft, ifft, fftshift
 from numpy import (
     nan_to_num,
+    any,
+    all,
     complex128,
     errstate,
     linspace,
@@ -16,6 +18,7 @@ from numpy import (
     vstack,
     array,
     zeros,
+    ones,
     empty,
     arange,
     around,
@@ -23,6 +26,7 @@ from numpy import (
     nanstd,
     angle,
     isnan,
+    isfinite,
     sqrt,
     roll,
     sum,
@@ -48,14 +52,29 @@ TODAY = datetime.utcnow()
 CALFILE = TODAY.strftime('/global/logs/vlbi_cal/vlbi_cal.%j-%Y.json')
 REDIS_PREFIX = 'swarm.calibrate_vlbi'
 
+
 def solve_cgains(mat, ref=0):
-    vals, vecs = eig(mat)
-    max_val = vals.real.max()
-    max_vec = vecs[:, vals.real.argmax()]
-    raw_gains = max_vec * sqrt(max_val).squeeze()
-    ref_gain = raw_gains[ref]
-    factor = ref_gain.conj() / abs(ref_gain + 1.0)
-    return raw_gains * nan_to_num(factor)
+    if mat.shape[0] == 1:
+        # If we only have one antenna, then this whole thing can be bypassed.
+        return ones([1], dtype=mat.dtype)
+
+    eig_vals, eig_vecs = eig(mat)
+    soln_idx = eig_vals.real.argmax()
+    gain_soln = (eig_vecs[:, soln_idx] * sqrt(eig_vals[soln_idx].real)).squeeze()
+
+    if isfinite(gain_soln[ref]):
+        # Apply reference antenna phase to the solutions
+        gain_soln *= exp(-1j*angle(gain_soln[ref]))
+
+        # Normalize to account for the fact the autos have been removed -- more
+        # specifically, the real components (since the imag should be zero).
+        gain_soln *= sqrt(2 * mat.size / ((2 * mat.size) - gain_soln.size))
+
+        # Avoid numerical precision issues, explicitly zero out phaes for refant
+        gain_soln[ref].imag = 0
+
+    return gain_soln
+
 
 def slice_sub_lags(lags, peaks, axis, max_lags=16):
     out_shape = array(lags.shape)
@@ -65,6 +84,7 @@ def slice_sub_lags(lags, peaks, axis, max_lags=16):
         indices = fftshift(list(range(peak-max_lags, peak+max_lags)))
         out_array.T[i] = lags.T[i].take(indices, mode='wrap')
     return out_array
+
 
 def solve_delay_phase(gains, chan_axis=0, sub_max_lags=16):
 
@@ -91,17 +111,29 @@ def solve_delay_phase(gains, chan_axis=0, sub_max_lags=16):
     phases = (180.0/pi) * angle(interp_lags.take(interp_peaks, axis=chan_axis)).diagonal()
     return delays, phases
 
+
 def complex_nan_to_num(arr):
-    out_arr = arr.copy()
-    out_arr[isnan(out_arr)] = 0.0j
-    return out_arr
+    # Modify the array in-situ
+    arr[isnan(arr)] = 0.0
+    return arr
+
 
 def wrap_phase(in_phase):
-        return (in_phase + 180.0) % 360.0 - 180.0
+    return (in_phase + 180.0) % 360.0 - 180.0
+
 
 class CalibrateVLBI(SwarmDataCallback):
 
-    def __init__(self, swarm, reference=None, normed=False, single_chan=True, history_size=8, PID_coeffs=(0.75, 0.05, 0.01), outfilename=CALFILE):
+    def __init__(
+        self,
+        swarm,
+        reference=None,
+        normed=False,
+        single_chan=True,
+        history_size=8,
+        PID_coeffs=(0.75, 0.05, 0.01),
+        outfilename=CALFILE,
+    ):
         if reference is None:
             self.reference = dict(list(zip(SWARM_BENGINE_SIDEBANDS,[None]*len(SWARM_BENGINE_SIDEBANDS))))
             for beam_per_quad in swarm.get_beamformer_inputs():
@@ -157,14 +189,22 @@ class CalibrateVLBI(SwarmDataCallback):
             self.skip_next[:] = True
         if not this_input==self.reference['USB']:
             self.swarm.set_delay(this_input, updated_delay)
-            self.logger.debug('{0} : Old delay={1:>8.2f} ns,  New delay={2:>8.2f} ns,  Diff. delay={3:>8.2f} ns'.format(this_input, current_delay, updated_delay, feedback_delay))
+            self.logger.debug(
+                '{0} : Old delay={1:>8.2f} ns,  New delay={2:>8.2f} ns,  Diff. delay={3:>8.2f} ns'.format(
+                    this_input, current_delay, updated_delay, feedback_delay
+                )
+            )
 
     def feedback_phase_usb(self, this_input, feedback_phase):
         current_phase = self.swarm.get_phase(this_input)
         updated_phase = current_phase + feedback_phase
         if not this_input==self.reference['USB']:
             self.swarm.set_phase(this_input, wrap_phase(updated_phase))
-            self.logger.debug('{0}:USB : Old phase={1:>8.2f} deg, New phase={2:>8.2f} deg, Diff. phase={3:>8.2f} deg'.format(this_input, current_phase, updated_phase, feedback_phase))
+            self.logger.debug(
+                '{0}:USB : Old phase={1:>8.2f} deg, New phase={2:>8.2f} deg, Diff. phase={3:>8.2f} deg'.format(
+                    this_input, current_phase, updated_phase, feedback_phase
+                )
+            )
 
     def feedback_phase_lsb(self, inputs, feedback_phases):
         # Do the phase update per quadrant
@@ -183,7 +223,10 @@ class CalibrateVLBI(SwarmDataCallback):
             # And set the phases
             self.swarm.set_beamformer_second_sideband_phase(these_inputs, updated_phases)
             for ii, this_input in enumerate(these_inputs):
-                self.logger.debug('{0}:LSB : Old phase={1:>8.2f} deg, New phase={2:>8.2f} deg, Diff. phase={3:>8.2f} deg'.format(this_input, current_phases[ii], updated_phases[ii], feedback_phases[ii]))
+                self.logger.debug(
+                    '{0}:LSB : Old phase={1:>8.2f} deg, New phase={2:>8.2f} deg, Diff. phase={3:>8.2f} deg'.format(
+                        this_input, current_phases[ii], updated_phases[ii], feedback_phases[ii])
+                    )
 
     def pid_servo(self, inputs, sb_idx):
         p, i, d = self.PID_coeffs
@@ -193,43 +236,60 @@ class CalibrateVLBI(SwarmDataCallback):
         pid_delays = p * p_delays + i * i_delays + d * d_delays
         pid_phases = p * p_phases + i * i_phases + d * d_phases
         if SWARM_BENGINE_SIDEBANDS[sb_idx] == 'USB':
-            if not isnan(pid_delays).any():
+            if all(isfinite(pid_delays)) and any(pid_delays):
                 list(map(self.feedback_delay_usb, inputs, pid_delays))
-            if not isnan(pid_phases).any():
+            if all(isfinite(pid_phases)) and any(pid_phases):
                 list(map(self.feedback_phase_usb, inputs, pid_phases))
         elif SWARM_BENGINE_SIDEBANDS[sb_idx] == 'LSB':
-            self.feedback_phase_lsb(inputs, pid_phases)
+            if all(isfinite(pid_phases)) and any(pid_phases):
+                self.feedback_phase_lsb(inputs, pid_phases)
 
-    def solve_for(self, data, inputs, chunk, pol, sideband='USB'):
+    def solve_for(self, data, inputs, chunk, pol, sideband='USB', edge_chan=1024):
         # Check if reference is in beam
         ref_in_beam = True
         this_reference = SwarmInput(self.reference[sideband].ant, chunk, pol)
+        ref_input = inputs.index(this_reference)
+
         if this_reference not in inputs:
-            self.logger.warn('Reference {ref!r} not in beam, adding it for the purpose of phase solution'.format(ref=this_reference))
+            self.logger.warn(
+                'Reference {ref!r} not in beam, adding it for the purpose of phase solution'.format(
+                    ref=this_reference
+                )
+            )
             inputs.append(this_reference)
             ref_in_beam = False
 
-        baselines = list(baseline for baseline in data.baselines if ((baseline.left in inputs) and (baseline.right in inputs)))
-        corr_matrix = zeros([SWARM_CHANNELS, len(inputs), len(inputs)], dtype=complex128)
+        baselines = list(
+            baseline for baseline in data.baselines if (
+                (baseline.left in inputs) and (baseline.right in inputs)
+            )
+        )
+
+        corr_matrix = zeros([len(inputs), len(inputs), SWARM_CHANNELS], dtype=complex128)
         for baseline in baselines:
             left_i = inputs.index(baseline.left)
             right_i = inputs.index(baseline.right)
-            baseline_data = data[baseline, sideband]
-            complex_data = baseline_data[0::2] + 1j * baseline_data[1::2]
-            corr_matrix[:, left_i, right_i] = complex_data
-            corr_matrix[:, right_i, left_i] = complex_data.conj()
-        referenced_solver = partial(solve_cgains, ref=inputs.index(this_reference))
+            if left_i == right_i:
+                # We don't want to consider the autos, since they are positively biased.
+                continue
+            baseline_data = data[baseline, sideband].view('<c8')
+            corr_matrix[left_i, right_i] = baseline_data
+            corr_matrix[right_i, left_i] = baseline_data.conj()
         if self.normed:
             with errstate(invalid='ignore'):
                 corr_matrix = complex_nan_to_num(corr_matrix / abs(corr_matrix))
-        if not self.single_chan:
-            full_spec_gains = array(self.map(referenced_solver, corr_matrix))
-            delays, phases = solve_delay_phase(full_spec_gains)
-        else:
-            full_spec_gains = array(self.map(referenced_solver, [corr_matrix.mean(axis=0),]))
-            phases = (180.0/pi) * angle(full_spec_gains.mean(axis=0))
+        if self.single_chan:
+            gains_soln = solve_cgains(
+                corr_matrix[:, :, edge_chan:-edge_chan].mean(axis=-1), ref=ref_input
+            )
+            phases = (180.0/pi) * angle(gains_soln)
             delays = zeros(len(inputs))
-        amplitudes = abs(full_spec_gains).mean(axis=0)
+            amplitudes = abs(gains_soln)
+        else:
+            referenced_solver = partial(solve_cgains, ref=ref_input)
+            gains_soln = array(self.map(referenced_solver, corr_matrix.transpose((2, 0, 1))))
+            delays, phases = solve_delay_phase(gains_soln)
+            amplitudes = abs(gains_soln).mean(axis=0)
 
         # Remove reference solution if it is not in beam
         if not ref_in_beam:
@@ -238,7 +298,7 @@ class CalibrateVLBI(SwarmDataCallback):
             amplitudes = amplitudes[:-1]
 
         with errstate(invalid='ignore'):
-            efficiency = (abs(full_spec_gains.sum(axis=1)) / abs(full_spec_gains).sum(axis=1)).real
+            efficiency = (abs(gains_soln.sum(axis=1)) / abs(gains_soln).sum(axis=1)).real
         return efficiency, vstack([amplitudes, delays, phases])
 
     def apply_beamformer_second_sideband_phase(self, data_bytes, sideband="LSB"):
@@ -291,11 +351,29 @@ class CalibrateVLBI(SwarmDataCallback):
             amplitudes, delays, phases = cal_solution
 
             for i in range(len(inputs)):
-                self.logger.debug('{}:{} : Amp={:>12.2e}, Delay={:>8.2f} ns, Phase={:>8.2f} deg'.format(inputs[i], sb_str, amplitudes[i], delays[i], phases[i]))
+                self.logger.debug(
+                    '{}:{} : Amp={:>12.2e}, Delay={:>8.2f} ns, Phase={:>8.2f} deg'.format(
+                        inputs[i], sb_str, amplitudes[i], delays[i], phases[i]
+                    )
+                )
             for ichunk, chunk in enumerate(listed_chunks):
                 for ipol, pol in enumerate(listed_pols):
-                    self.logger.info('Avg. phasing efficiency across chunk {}, pol {}, sideband {}={:>8.2f} +/- {:.2f}'.format(chunk, pol, sb_str, nanmean(efficiencies[ipol][ichunk]), nanstd(efficiencies[ipol][ichunk])))
-                    self.redis.setex('{0}.efficiency.chk{1}.pol{2}.{3}'.format(REDIS_PREFIX, chunk, pol, sb_str), int(data.int_length*2), efficiencies[ipol][ichunk][0])
+                    self.logger.info(
+                        'Avg. phasing efficiency across chunk {}, pol {}, sideband {}={:>8.2f} +/- {:.2f}'.format(
+                            chunk,
+                            pol,
+                            sb_str,
+                            nanmean(efficiencies[ipol][ichunk]),
+                            nanstd(efficiencies[ipol][ichunk]),
+                        )
+                    )
+                    self.redis.setex(
+                        '{0}.efficiency.chk{1}.pol{2}.{3}'.format(
+                            REDIS_PREFIX, chunk, pol, sb_str
+                        ),
+                        int(data.int_length*2),
+                        efficiencies[ipol][ichunk][0],
+                    )
 
             if self.inputs[sb_idx] != inputs:
                 self.init_history(cal_solution,sb_idx)
@@ -327,7 +405,9 @@ class CalibrateVLBI(SwarmDataCallback):
                     'inputs_lsb': list((inp.ant, inp.chk, inp.pol) for inp in inputs_lsb),
                     'efficiencies_usb': list(nanmean(eff) for pol_eff in efficiencies_usb for eff in pol_eff),
                     'efficiencies_lsb': list(nanmean(eff) for pol_eff in efficiencies_lsb for eff in pol_eff),
-                    'delays_usb': new_delays_usb, 'phases_usb': new_phases_usb, 'phases_lsb': new_phases_lsb,
+                    'delays_usb': new_delays_usb,
+                    'phases_usb': new_phases_usb,
+                    'phases_lsb': new_phases_lsb,
                     'cal_solution_usb': cal_solution_usb.tolist(),
                     'cal_solution_lsb': cal_solution_lsb.tolist(),
                     })
